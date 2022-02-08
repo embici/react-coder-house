904dc77a914dfe26037b39e9211fae6db44ef84c
Esteban Montero <estebanmontero@Estebans-MacBook-Air.local>
2022-02-07 22:15:22 -0600

    Product Detailed View is rendering now

--- project/embici_store/src/components/ItemDetail.js
+++ project/embici_store/src/components/ItemDetail.js
@@ -4,20 +4,35 @@
 import { useEffect, useState } from "react";
 
 const ItemDetail = ({product}) => {
-    // const [detailProduct, setDetailProduct] = useState([]);
+    const [detailProduct, setDetailProduct] = useState([]);
 
     useEffect(() =>{ 
-        // setDetailProduct(product);
+        setDetailProduct(product);
         console.log('Product Detailed ----->>>> ', product);
     }, [product]);
 
     return(
-        <section className="container mx-auto flex flex-row">
-            {product.length > 0 ? (<><img alt={product.name} className="md:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.img} /><div className="md:w-1/2 w-full">
-                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name} </h1>
-                <p>USD ${product.price} </p>
-                <p>QTY {product.stock} </p>
-            </div></>) : <p>Cargando ...</p>}
+        <section className="">
+            {
+                detailProduct.map(productItem => { 
+                    return (
+                        (<div key={productItem.id} className="container mx-auto flex flex-wrap">
+                            <img alt={productItem.name} className="md:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={productItem.img} />
+                            <div className="md:w-1/2 w-full px-5">
+                                <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{productItem.name} </h1>
+                                <p>USD ${productItem.price} </p>
+                                <p>QTY {productItem.stock} </p>
+                            </div>
+                        </div>)
+                    )
+                })
+                
+            // product.length > 0 ? (<><img alt={product.name} className="md:w-1/2 w-full object-cover object-center rounded border border-gray-200" src={product.img} /><div className="md:w-1/2 w-full">
+            //     <h1 className="text-gray-900 text-3xl title-font font-medium mb-1">{product.name} </h1>
+            //     <p>USD ${product.price} </p>
+            //     <p>QTY {product.stock} </p>
+            // </div></>) : <p>Cargando ...</p>}
+            }           
         </section>
     )
 }
